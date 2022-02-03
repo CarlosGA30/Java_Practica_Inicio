69b2d09b082661d719bed65da57593f4156049c7
CarlosGA30 <garcia.arriaga.jose.carlos@gmail.com>
2022-02-03 10:17:31 -0600

    datos

---
+++ JavaBasicos/src/metodos/Metodos.java
@@ -0,0 +1,11 @@
+package metodos;
+
+public class Metodos {
+	int a,b;//atributos
+	
+	public void sumar() {
+		int resultado=a+b;
+		System.out.println("La suma de "+a+" y "+b+" es "+resultado);
+	}
+	
+}
---
+++ JavaBasicos/src/metodos/PruebaMetodos.java
@@ -0,0 +1,13 @@
+package metodos;
+
+public class PruebaMetodos {
+	
+	public static void main(String[] args) {
+		Metodos metodoPrueba=new Metodos();
+		metodoPrueba.a=10;
+		metodoPrueba.b=67;
+		metodoPrueba.sumar();
+	}
+	
+	
+}
